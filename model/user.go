fa0dce2990e27e4920d97ede78919833040e0714
koyo <nexus9071@gmail.com>
2018-11-09 03:48:31 +0900

    userIDをプライマリーキーに

--- model/user.go
+++ model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 // User モデルの定義
 type User struct {
-	ID        uint64    `json:"id"`
+	ID        uint64    `json:"id" gorm:"primary_key"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
